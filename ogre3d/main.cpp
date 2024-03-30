5bf5d392fc294dbddcf36ed9eb20dff82e13d979
netpipe <teslaenergy@gmail.com>
2024-03-30 10:03:21 -0600

    testing

--- ogre3d/main.cpp
+++ ogre3d/main.cpp
@@ -1,95 +1,81 @@
-#include <Ogre.h>
 #include <SDL2/SDL.h>
-#include <iostream>
-#include <OgreConfigDialog.h> // Include OgreConfigDialog.h for ConfigDialog
+#include <OgreRoot.h>
+#include <OgreRenderWindow.h>
+#include <OgreSceneManager.h>
+#include <OgreEntity.h>
+#include <OgreCamera.h>
+#include <OgreViewport.h>
 #include <SDL2/SDL_syswm.h>
+
 int main(int argc, char* argv[]) {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
-        return 1;
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  // Create SDL window
+  SDL_Window* window = SDL_CreateWindow("SDL2 & Ogre3D Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+  if (window == nullptr) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  // Initialize Ogre
+  Ogre::Root* root = new Ogre::Root();
+  root->loadPlugin("RenderSystem_GL"); // Load OpenGL RenderSystem
+  Ogre::RenderSystem* renderSystem = root->getRenderSystemByName("OpenGL Rendering Subsystem");
+  root->setRenderSystem(renderSystem);
+  root->initialise(false);
+
+  SDL_SysWMinfo sdlInfo;
+  SDL_VERSION(&sdlInfo.version);
+  SDL_GetWindowWMInfo(window, &sdlInfo);
+
+  // Create render window with SDL window handle
+  Ogre::NameValuePairList params;
+  //params["externalWindowHandle"] = Ogre::StringConverter::toString(reinterpret_cast<size_t>(sdlInfo.info.x11.window));
+
+  // Create Ogre window
+  //Ogre::NameValuePairList params;
+  params["externalWindowHandle"] = Ogre::StringConverter::toString((size_t)sdlInfo.info.x11.window);
+ // params["externalWindowHandle"] = Ogre::StringConverter::toString((size_t)window);
+  Ogre::RenderWindow* ogreWindow = root->createRenderWindow("OgreWindow", 800, 600, false, &params);
+
+  // Create scene
+  Ogre::SceneManager* sceneManager = root->createSceneManager();
+  //Ogre::Entity* cubeEntity = sceneManager->createEntity("Cube", "./test.3ds");
+ // Ogre::SceneNode* cubeNode = sceneManager->getRootSceneNode()->createChildSceneNode();
+ // cubeNode->attachObject(cubeEntity);
+
+  // Create camera and viewport
+  Ogre::Camera* camera = sceneManager->createCamera("Camera");
+//  camera->setPosition(Ogre::Vector3(0, 0, 50));
+ // camera->lookAt(Ogre::Vector3(0, 0, 0));
+  Ogre::Viewport* viewport = ogreWindow->addViewport(camera);
+  viewport->setBackgroundColour(Ogre::ColourValue(0.5, 0.5, 0.5));
+
+  // Main loop
+  bool running = true;
+  while (running) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        running = false;
+      }
     }
+//see
+    // Update scene
+  //  cubeNode->yaw(Ogre::Degree(0.1f));
 
-    // Create SDL window
-    SDL_Window* sdlWindow = SDL_CreateWindow("Ogre3D Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    if (!sdlWindow) {
-        std::cerr << "SDL window creation failed: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
+    // Render frame
+    root->renderOneFrame();
+  }
 
-    // Initialize Ogre3D
-    Ogre::Root root;
-    Ogre::ConfigDialog *configDialog;
-    if (!root.showConfigDialog(configDialog)) {
-        return 1; // Dialog canceled
-    }
-    root.restoreConfig();
+  // Cleanup
+  delete root;
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
-    // Get SDL window handle
-    SDL_SysWMinfo sdlInfo;
-    SDL_VERSION(&sdlInfo.version);
-    SDL_GetWindowWMInfo(sdlWindow, &sdlInfo);
-
-    // Create render window with SDL window handle
-    Ogre::NameValuePairList params;
-    params["externalWindowHandle"] = Ogre::StringConverter::toString(reinterpret_cast<size_t>(sdlInfo.info.x11.window));
-    Ogre::RenderWindow* renderWindow = root.createRenderWindow("Ogre3D Demo", 800, 600, false, &params);
-
-    // Create scene manager
-    Ogre::SceneManager* sceneMgr = root.createSceneManager();
-
-    // Create camera
-    Ogre::Camera* camera = sceneMgr->createCamera("MainCamera");
-   // camera->setPosition(0, 0, 500);
-   // camera->lookAt(0, 0, 0);
-
-    // Create viewport
-    Ogre::Viewport* viewport = renderWindow->addViewport(camera);
-    viewport->setBackgroundColour(Ogre::ColourValue(0.0f, 0.0f, 0.0f));
-
-    // Create cube entity
-    Ogre::Entity* cubeEntity = sceneMgr->createEntity("Cube", Ogre::SceneManager::PT_CUBE);
-    Ogre::SceneNode* cubeNode = sceneMgr->getRootSceneNode()->createChildSceneNode();
-    cubeNode->attachObject(cubeEntity);
-
-    // Main loop flag
-    bool quit = false;
-
-    // Main loop
-    while (!quit) {
-        // Handle events
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-
-        // Rotate cube
-        cubeNode->yaw(Ogre::Degree(0.5f));
-
-        // Clear viewport
-    //    renderWindow->clear();
-
-        // Render scene
-        root.renderOneFrame();
-
-        // Present the rendered image
-        renderWindow->update();
-
-        // Delay to control frame rate
-        SDL_Delay(10);
-    }
-
-    // Destroy Ogre3D resources
-    root.destroySceneManager(sceneMgr);
-
-    // Destroy SDL window
-    SDL_DestroyWindow(sdlWindow);
-
-    // Quit SDL
-    SDL_Quit();
-
-    return 0;
+  return 0;
 }
