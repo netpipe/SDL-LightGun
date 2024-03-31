71995ab9b2a82fa984bd7c7dd1b937d5ee7a79e7
netpipe <teslaenergy@gmail.com>
2024-03-31 11:13:12 -0600

    fix

--- ogre3d/build.sh
+++ ogre3d/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ main.cpp -lOgreMain -lSDL2_mixer -lSDL2 -lSDL2_image -lGL -lOgreOverlay -L./ogre/lib -I./ogre/Samples/Common/include -I./ogre/OgreMain/include -I./ogre/include -I./ogre/Components/Overlay/include/ -I./ogre/Components/Bites/include/
+g++ main.cpp -lOgreMain -lSDL2_mixer -lSDL2 -lSDL2_image -lGL -lOgreOverlay -L./ogre/lib -I./ogre/Samples/Common/include -I./ogre/OgreMain/include -I./ogre/include -I./ogre/Components/Overlay/include/ -I./ogre/Components/Bites/include/ -o flow
 
 #-I/usr/local/include/OGRE/Overlay
 #-I/usr/local/include/OGRE/Bites
